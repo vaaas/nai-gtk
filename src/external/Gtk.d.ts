bc013960c9ea982af70cf6a7ae4d4db983e82b8c
Vasileios Pasialiokis <pasivasi@outlook.com>
2022-06-07 16:37:46 +0300

    additions

--- src/external/Gtk.d.ts
+++ src/external/Gtk.d.ts
@@ -1,3 +1,5 @@
+type ValueOf<T> = T[keyof T]
+
 export type ApplicationEvent = 'activate'
 
 declare class Application {
@@ -13,17 +15,62 @@ declare class ApplicationWindow {
 	close(): void
 }
 
-declare class Widget {}
+declare class Widget {
+	add_css_class(x: string): void
+}
 
 declare class Button extends Widget {
 	constructor(x: { label: string })
 	connect: (event: 'clicked', callback: () => void) => void
 }
 
+declare class Box extends Widget {
+	constructor(x: { orientation: ValueOf<Orientation>, spacing: number })
+	append(x: Widget): void
+}
+
+declare class ListBox extends Widget {
+	constructor()
+	append(x: Widget): void
+}
+
+declare class Grid extends Widget {
+	constructor()
+	attach(child: Widget, column: number, row: number, width: number, height: number): void
+	set_column_homogeneous(x: boolean): void
+	set_column_spacing(x: number): void
+	set_row_spacing(x: number): void
+}
+
+declare class Entry extends Widget {
+	constructor()
+}
+
+declare class PasswordEntry extends Widget {
+	constructor()
+}
+
+declare class Label extends Widget {
+	constructor(x: { label: string })
+}
+
+type Orientation = {
+	readonly HORIZONTAL: unique symbol,
+	readonly VERTICAL: unique symbol,
+}
+
 interface Gtk {
 	Application: typeof Application,
 	ApplicationWindow: typeof ApplicationWindow,
+	Widget: typeof Widget,
 	Button: typeof Button,
+	Box: typeof Box,
+	ListBox: typeof ListBox,
+	Label: typeof Label,
+	Entry: typeof Entry,
+	PasswordEntry: typeof PasswordEntry,
+	Grid: typeof Grid,
+	Orientation: Orientation,
 }
 
 export {
@@ -31,5 +78,12 @@ export {
 	ApplicationWindow,
 	Widget,
 	Button,
+	Box,
+	ListBox,
+	Label,
+	Entry,
+	PasswordEntry,
+	Grid,
+	Orientation,
 	Gtk,
 }
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,28 @@ const Gtk = imports.gi.Gtk
 type Application = import('./external/Gtk.js').Application
 
 function on_activate(app: Application) {
-	// … create a new window …
 	const win = new Gtk.ApplicationWindow({ application: app })
 
-	// … with a button in it …
-	const btn = new Gtk.Button({ label: 'Hello, World!' })
-
-	// … which closes the window when clicked
+	const btn = new Gtk.Button({ label: 'Login' })
 	btn.connect('clicked', () => { win.close() })
-	win.set_child(btn)
+	btn.add_css_class('suggested-action')
+
+	const username_label = new Gtk.Label({ label: 'Username' })
+	const username_entry = new Gtk.Entry()
+
+	const password_label = new Gtk.Label({ label: 'Password' })
+	const password_entry = new Gtk.PasswordEntry()
+
+	const grid = new Gtk.Grid()
+	grid.attach(username_label, 1, 1, 1, 1)
+	grid.attach(username_entry, 2, 1, 1, 1)
+	grid.attach(password_label, 1, 2, 1, 1)
+	grid.attach(password_entry, 2, 2, 1, 1)
+	grid.attach(btn, 1, 3, 2, 1)
+	grid.set_row_spacing(10)
+	grid.set_column_spacing(10)
+
+	win.set_child(grid)
 	win.present()
 }
 
